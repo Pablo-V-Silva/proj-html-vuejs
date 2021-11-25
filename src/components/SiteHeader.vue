d2bbc99017c56500e84340804a37f110c6ed5d9b
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-11-25 17:42:49 +0100

    header complete, for real
    -jumbo
    -nav
    -card dei servizi

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,5 +1,6 @@
 <template>
   <header>
+    <!-- Numero verde -->
     <div class="contacts">
       <div class="container d-flex justify-content-between py-2">
         <div class="successfullRate">We have 95% Successful Pass Rate</div>
@@ -9,11 +10,17 @@
         </div>
       </div>
     </div>
+
+    <!-- immagine di sfondo jumbo -->
     <div class="jumbo">
+      <!-- navbar dinamica -->
       <div class="links">
         <nav class="navbar navbar-expand-lg navbar-dark">
           <div class="container py-3">
+            <!-- logo azienda -->
             <img src="../assets/img/avada-drivers-logo-1x.png" alt="" />
+
+            <!-- hamburger-menu -->
             <button
               class="navbar-toggler"
               type="button"
@@ -25,11 +32,14 @@
             >
               <span class="navbar-toggler-icon"></span>
             </button>
+
+            <!-- elementi della navbar -->
             <div
               class="collapse navbar-collapse justify-content-end"
               id="navbarNav"
             >
               <ul class="navbar-nav align-items-center">
+                <!-- link in links / link.link elemento del nav -->
                 <li
                   class="nav-item d-flex align-items-center"
                   v-for="link in links"
@@ -41,6 +51,8 @@
                   <span v-if="link.btn" class="newItem rounded-2">NEW</span>
                 </li>
               </ul>
+
+              <!-- ordinazione del libro di sicurezza alla guida -->
               <button type="button" class="btn rounded-pill greenBtn py-1">
                 BOOK NOW
               </button>
@@ -49,11 +61,26 @@
         </nav>
       </div>
 
+      <!-- frase motivazionale dell'azienda -->
       <div class="vignet container">
         <h1 class="text-light">Drive width Avada</h1>
         <p class="text-light">
           We offer the finest driving tuition money can buy
         </p>
+
+        <!-- bottoni per la selezione dei servizi dell'azienda messi in position fixed per averci sempre accesso -->
+        <div class="products">
+          <div class="cardproduct">
+            <img src="../assets/img/demoss.svg" alt="" class="bg-light" />
+            <div class="">Demos</div>
+          </div>
+          <div class="cardproduct mt-2">
+            <div class="green_text">
+              <span class="costValue">$</span><span class="fs-4">39</span>
+            </div>
+            <div class="">On Sale</div>
+          </div>
+        </div>
       </div>
     </div>
   </header>
@@ -106,6 +133,29 @@ export default {
 
   .vignet {
     padding: 9rem 0;
+
+    .products {
+      position: fixed;
+      right: 1rem;
+      top: 7.7rem;
+
+      .cardproduct {
+        width: 65px;
+        padding: 0.3em;
+        background: white;
+        text-align: center;
+        border-radius: 5px;
+
+        .green_text {
+          color: $principal_color;
+
+          .costValue {
+            position: relative;
+            bottom: 0.5em;
+          }
+        }
+      }
+    }
   }
 }
 </style>
