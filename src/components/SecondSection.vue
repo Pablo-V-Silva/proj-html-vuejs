3370c82d64b8bc3b71d1969a816f49caf7a5a8f3
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-11-26 17:58:30 +0100

    banner of second section complete

---
+++ src/components/SecondSection.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="bannerlearning">
+      <div class="container">
+        <div class="row row-cols-2">
+          <div class="col">
+            <h3 class="text-capitalize text-light">
+              start learning <span class="text-lowercase">to</span> drive today
+            </h3>
+            <div class="text-light">
+              Give us a call to schedule your first driving lessons
+            </div>
+          </div>
+          <div class="col d-flex justify-content-center align-items-center">
+            <div class="text-light fs-2">
+              <i class="fas fa-phone me-3"></i> 1-800-555-555
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.bannerlearning {
+  background-image: url("../assets/img/horizontal-cta-background.jpg");
+  background-size: cover;
+  background-position: center;
+  padding: 4rem 0;
+}
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,14 +1,17 @@
 <template>
   <main>
     <FirstSection />
+    <SecondSection />
   </main>
 </template>
 
 <script>
 import FirstSection from "./FirstSection.vue";
+import SecondSection from "./SecondSection.vue";
 export default {
   components: {
     FirstSection,
+    SecondSection,
   },
 };
 </script>
