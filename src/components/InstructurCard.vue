f2b0cb69cafc5adeef7391d12358893d46d950f0
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-11-29 08:25:17 +0100

    add border-top on instructors cards

--- src/components/InstructurCard.vue
+++ src/components/InstructurCard.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="row row-cols-3 g-5 instructorsContain">
     <div class="col" v-for="instructor in instructors" :key="instructor.name">
-      <div
-        class="instructorCard d-flex flex-column align-items-center card"
-        :class="instructor.center ? 'center' : ''"
-      >
-        <img
-          :src="require(`../assets/img/${instructor.image}`)"
-          alt=""
-          width="80%"
-          class="py-4"
-        />
-        <h4>{{ instructor.name }}</h4>
-        <div class="social py-2">
-          <i :class="instructor.firstsocial" class="px-2 fs-4"></i>
-          <i :class="instructor.secondsocial" class="px-2 fs-4"></i>
-          <i :class="instructor.thirdsocial" class="px-2 fs-4"></i>
+      <div class="instructorCard">
+        <div
+          class="forBorder d-flex flex-column align-items-center"
+          :class="instructor.center ? 'center' : ''"
+        >
+          <img
+            :src="require(`../assets/img/${instructor.image}`)"
+            alt=""
+            width="80%"
+            class="py-4"
+          />
+          <h4>{{ instructor.name }}</h4>
+          <div class="social py-2">
+            <i :class="instructor.firstsocial" class="px-2 fs-4"></i>
+            <i :class="instructor.secondsocial" class="px-2 fs-4"></i>
+            <i :class="instructor.thirdsocial" class="px-2 fs-4"></i>
+          </div>
+          <p class="mx-5 text-center">{{ instructor.speech }}</p>
         </div>
-        <p class="mx-5 text-center">{{ instructor.speech }}</p>
       </div>
     </div>
   </div>
@@ -68,10 +70,17 @@ export default {
     -webkit-box-shadow: 0px 30px 50px 14px rgba(0, 0, 0, 0.4);
     box-shadow: 0px 30px 50px 14px rgba(0, 0, 0, 0.4);
     min-height: 400px;
+    border-top: 7px solid $principal_color;
+    border-radius: 1rem;
 
-    &.center {
-      min-height: 420px;
+    .forBorder {
+      background-color: white;
+      border-radius: 1rem;
+      &.center {
+        min-height: 420px;
+      }
     }
+
     img {
       border-radius: 30px;
     }
